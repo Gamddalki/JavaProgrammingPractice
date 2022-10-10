2c007448e8ceb68cf3223c08434b73e28044ab5c
Gamddalki <hannahk01@naver.com>
2022-10-10 23:32:32 +0900

    Create Calculator.java
    추상클래스의 구현 연습 - 슈퍼클래스(추상)

---
+++ 0928/src/Calculator.java
@@ -0,0 +1,11 @@
+/*
+ * 소스파일: Calculator.java
+ * 이화여대 컴퓨터공학전공 2071019 김한나
+ * 추상클래스의 구현 연습 - 슈퍼클래스(추상)
+ */
+
+abstract class Calculator {
+	public abstract int add(int a, int b);
+	public abstract int sub(int a, int b);
+	public abstract double ave(int []a);
+}
