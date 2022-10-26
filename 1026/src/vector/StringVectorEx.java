731ab8f9f9788c11a94896e93b09a1f0a661173a
Gamddalki <hannahk01@naver.com>
2022-10-26 16:39:20 +0900

    Create StringVectorEx.java
    문자열 입력받아 Vector<String>에 저장해 길이 비교 후 출력

---
+++ 1026/src/vector/StringVectorEx.java
@@ -0,0 +1,49 @@
+/*
+ * 소스파일: StringVectorEx.java
+ * 이화여대 컴퓨터공학전공 2071019 김한나
+ * 문자열 입력받아 Vector<String>에 저장해 길이 비교 후 출력
+ */
+
+package vector;
+import java.util.Vector;
+import java.util.Scanner;
+
+public class StringVectorEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vector<String> v = new Vector<String>();		//문자열만 삽입 가능한 제네릭 벡터 생성
+		
+		Scanner scanner = new Scanner(System.in);
+		for(int i=0; i<4; i++) {
+			System.out.print("이름을 입력하세요>>");
+			String s = scanner.nextLine();	//띄어쓰기 포함해 입력받기
+			v.add(s);			//키보드로부터 4개의 이름을 입력받아 요소로 삽입
+		}
+		
+		//모든 이름 출력
+		for(int i=0; i<v.size(); i++) {
+			String name = v.get(i);
+			System.out.print(name + " ");
+		}
+		
+		//가장 긴 이름 찾기
+		int longestIndex = 0;
+		for(int i=1; i<v.size(); i++) {
+			if(v.get(longestIndex).length()<v.get(i).length())
+				longestIndex=i;
+		}
+		
+		//가장 짧은 이름 찾기
+		int shortestIndex = 0;
+		for(int i=1; i<v.size(); i++) {
+			if(v.get(shortestIndex).length()>v.get(i).length())
+				shortestIndex=i;
+		}
+		
+		//가장 긴 이름과 가장 짧은 이름 출력
+		System.out.println("\n\n가장 긴 이름은 : " + v.get(longestIndex));
+		System.out.println("가장 짧은 이름은 : " + v.get(shortestIndex));
+	}
+
+}
