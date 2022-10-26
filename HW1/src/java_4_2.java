2e373beab8ca43f79ac3c16a6a06db0654791810
Gamddalki <hannahk01@naver.com>
2022-10-26 16:11:19 +0900

    Create java_4_2.java

---
+++ HW1/src/java_4_2.java
@@ -0,0 +1,42 @@
+/*
+ * 소스파일: java_4_2.java
+ * 이화여대 컴퓨터공학전공 2071019 김한나
+ * 간단한 클래스 만들기, scanner 활용
+ */
+
+import java.util.Scanner;
+
+class Grade {		//Grade 클래스
+	private int math, science, english;
+	
+	public Grade(int math, int science, int english) {		//x,y,width,height을 매개변수로 받아 필드를 초기화하는 생성자
+		this.math=math;
+		this.science=science;
+		this.english=english;
+	}
+	
+	public int average() {		//성적 평균을 리턴하는 함수
+		return(math+science+english)/3;
+	}
+}
+
+public class java_4_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		//전공 학번 이름 출력
+		System.out.println("이화여대 컴퓨터공학전공 2071019 김한나");
+		
+		System.out.print("수학, 과학, 영어 순으로 3개의 점수 입력>>");
+		int math = scanner.nextInt();
+		int science = scanner.nextInt();
+		int english = scanner.nextInt();
+		
+		Grade me = new Grade(math, science, english);
+		System.out.println("평균은 " + me.average());
+		
+		scanner.close();
+	}
+
+}
