b9a51fc69a55bb88c1aa3ab990cf2ef1d0bc51ea
Gamddalki <hannahk01@naver.com>
2022-11-23 15:05:42 +0900

    Update FileWriterEx.java

--- 1109/src/reader/FileWriterEx.java
+++ 1109/src/reader/FileWriterEx.java
@@ -17,13 +17,13 @@ public class FileWriterEx {
 		int c;
 		
 		try {
-			fout = new FileWriter("C:\\\\Users\\\\LG\\\\Desktop\\\\JavaProgrammingPractice\\\\1109\\\\src\\\\reader\\\\sample.txt");
+			fout = new FileWriter("C:\\\\Users\\\\LG\\\\Desktop\\\\JavaProgrammingPractice\\\\1109\\\\src\\\\reader\\\\sample.txt");	//실행 결과 sample.txt 파일 생성
 			while(true) {
 				String line = scanner.nextLine();
 				if(line.length() == 0)
 					break;
-				fout.write(line, 0, line.length());
-				fout.write("\r\n", 0, 2);
+				fout.write(line, 0, line.length());		//블록 단위 쓰기
+				fout.write("\r\n", 0, 2);		//한 줄을 띄우기 위해 \r\n을 파일에 저장
 			}
 			fout.close();
 		}
